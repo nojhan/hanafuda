3c47d3d5265153622cd762196a4182a3523b5ebe
nojhan <nojhan@nojhan.net>
2014-08-31 21:17:42 +0200

    More cleaner verbose log

--- export.sh
+++ export.sh
@@ -5,13 +5,17 @@ for svg in `find $1 -name *.svg` ; do
 
     # the complete filename without the extension
     base=`echo $svg | sed s/.svg//`
-    
+
     # export each 4 cards
     for i in `seq 4` ; do
-        inkscape --export-png=${base}_card_$i.png --without-gui --export-id=card_$i --export-height=800 $svg
+        echo "===== ${base}_card_$i ====="
+        time inkscape --export-png=${base}_card_$i.png --without-gui --export-id=card_$i --export-height=800 $svg
+        echo "===== DONE ====="
     done
 
     # export the whole page
-    inkscape --export-png=$base.png --export-area-page --export-width=1024 --without-gui $svg
+    echo "===== ${base} ====="
+    time inkscape --export-png=$base.png --export-area-page --export-width=1024 --without-gui $svg
+    echo "===== DONE ====="
 done
 
